9628246325485f3bec706b444e2eae957fbb16b6
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-06-28 14:49:27 -0600

    Add error handler, and cors middleware

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const {logger} = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
-const {logger} = require('./middleware/logEvents')
 
 // Custom middleware logger
 app.use(logger)
+
+// Cross Origin Resource Sharing
+const whitelist = ['http://127.0.0.1:5500', 'http//localhost:3500'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin ) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions))
 
 // Built-in middleware to handle urlencoded data
 // in other words: form data : 'content-type: application/x-www.for.urlencoded
@@ -27,6 +43,7 @@ app.get('/new-page(.html)?', (req, res) => {
 app.get('/old-page(.html)?', (req, res) => {
   res.redirect(301, '/new-page.html'); //302 by default
 })
+
 // Route handlers
 app.get('/hello(.html)?', (req, res, next) => {
   console.log('attempted to load hello.html');
@@ -51,11 +68,11 @@ const three = (req, res) => {
 }
 app.get('/chain(.html)?', [one, two, three])
 
-app.get('/*', (req, res) => {
+app.all('*', (req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
-
+app.use(errorHandler)
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
